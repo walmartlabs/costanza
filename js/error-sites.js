4680336553ee3c9e137417aea359654d2f7859db
kpdecker <kpdecker@gmail.com>
2013-12-03 01:27:15 -0600

    Initial ErrorSites impl

---
+++ js/error-sites.js
@@ -0,0 +1,5 @@
+this.ErrorSites = (function() {
+
+  return {
+  };
+})();
---
+++ test/js/error-sites.js
@@ -0,0 +1,22 @@
+/*global ErrorSites */
+describe('error-sites', function() {
+  describe('#onerror', function() {
+  });
+
+  describe('setTimeout', function() {
+    it('should catch errors');
+    it('should include current catch tag');
+  });
+  describe('setInterval', function() {
+    it('should catch errors');
+    it('should include current catch tag');
+  });
+  describe('addEventListener', function() {
+    it('should catch errors');
+    it('should include current catch tag');
+  });
+  describe('on attribute handlers', function() {
+    it('should catch errors');
+    it('should include current catch tag');
+  });
+});
