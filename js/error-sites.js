56b8f7d4ad4d54c82ec98cd911da5a8156349e44
kpdecker <kpdecker@gmail.com>
2013-12-04 22:11:50 -0600

    Implement ErrorSite.section

--- js/error-sites.js
+++ js/error-sites.js
@@ -1,12 +1,38 @@
 this.ErrorSites = (function() {
-  var reportCallback;
+  var reportCallback,
+      currentSection;
 
   function init(_reportCallback) {
     reportCallback = _reportCallback;
 
   }
 
+  function section(name, callback) {
+    if (!callback) {
+      callback = name;
+      name = currentSection;
+    }
+
+    return function() {
+      var priorSite = currentSection;
+
+      try {
+        currentSection = name;
+
+        callback.apply(this, arguments);
+      } catch (err) {
+        reportCallback(currentSection, err);
+      } finally {
+        currentSection = priorSite;
+      }
+    };
+  }
+
   return {
     init: init,
+    current: function() {
+      return currentSection;
+    },
+    section: section,
   };
 })();
--- test/js/error-sites.js
+++ test/js/error-sites.js
@@ -7,6 +7,43 @@ describe('error-sites', function() {
     ErrorSites.init(spy);
   });
 
+  describe('#section', function() {
+    it('should run callback', function() {
+      var callback = this.spy(),
+          section = ErrorSites.section(callback);
+      section();
+      expect(callback).to.have.been.calledOnce;
+
+      section = ErrorSites.section('foo', callback);
+      section();
+      expect(callback).to.have.been.calledTwice;
+    });
+    it('report errors', function() {
+      var section = ErrorSites.section('fail!', function() {
+        throw new Error('Failure is always an option');
+      });
+      section();
+      expect(spy).to.have.been.calledWith('fail!', new Error('Failure is always an option'));
+    });
+    it('should restore the site', function() {
+      var section1 = ErrorSites.section('success', function() {
+        expect(ErrorSites.current()).to.equal('success');
+        section2();
+        expect(ErrorSites.current()).to.equal('success');
+      });
+      var section2 = ErrorSites.section(function() {
+        expect(ErrorSites.current()).to.not.exist;
+        section3();
+      });
+      var section3 = ErrorSites.section('fail!', function() {
+        expect(ErrorSites.current()).to.equal('fail!');
+        throw new Error('Failure is always an option');
+      });
+      section1();
+      expect(spy).to.have.been.calledWith('fail!', new Error('Failure is always an option'));
+    });
+  });
+
   describe('#onerror', function() {
   });
 
