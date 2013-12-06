545813b9326dadc268d1b32aa7cc28edbb7f94e5
kpdecker <kpdecker@gmail.com>
2013-12-05 23:43:17 -0600

    Implement thorax binding

---
+++ js/costanza-thorax.js
@@ -0,0 +1,31 @@
+function bindEventHandler(eventName, params) {
+  eventName += params.originalName;
+
+  var callback = params.handler,
+      method = _.isFunction(callback) ? callback : this[callback];
+  if (!method) {
+    throw new Error('Event "' + callback + '" does not exist ' + (this.name || this.cid) + ':' + eventName);
+  }
+
+  var context = params.context || this,
+      section = 'thorax-exception: ' + (context.name || context.cid) + ':' + eventName,
+      ret = Costanza.section(section, _.bind(method, context));
+
+  // Backbone will delegate to _callback in off calls so we should still be able to support
+  // calling off on specific handlers.
+  ret._callback = method;
+  ret._thoraxBind = true;
+  return ret;
+}
+
+Thorax.onException = function(name, error) {
+  error = error || {};
+
+  Costanza.emit({
+      type: 'javascript',
+      section: name,
+      msg: error.message,
+      stack: error.stack
+    },
+    error);
+};
---
+++ test/js/costanza-thorax.js
@@ -0,0 +1,42 @@
+/*global Costanza */
+describe('costanza-thorax', function() {
+  var error = new Error('It failed!'),
+      spy;
+  beforeEach(function() {
+    spy = this.spy();
+    Costanza.init(spy);
+  });
+  afterEach(function() {
+    Costanza.cleanup();
+  });
+
+  it('should create a section for view events', function() {
+    var view = new (Thorax.View.extend({
+      name: 'test',
+      events: {
+        thorax: function() {
+          throw error;
+        }
+      }
+    }))();
+
+    view.trigger('thorax');
+    expect(spy).to.have.been.calledWith({
+        type: 'javascript',
+        section: 'thorax-exception: test:view-event:thorax',
+        msg: 'It failed!',
+        stack: error.stack
+      },
+      error);
+  });
+  it('should tie to onException', function() {
+    Thorax.onException('here!', error);
+    expect(spy).to.have.been.calledWith({
+        type: 'javascript',
+        section: 'here!',
+        msg: 'It failed!',
+        stack: error.stack
+      },
+      error);
+  });
+});
